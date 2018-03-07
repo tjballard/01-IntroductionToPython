8ab524c9e08fcba80d4d02245527469127577df5
ballartj <ballartj@rose-hulman.edu>
2018-03-06 22:50:55 -0500

    Hi.

--- src/m5_summary.py
+++ src/m5_summary.py
@@ -2,15 +2,15 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and TJ Ballard.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   Write code that constructs a SimpleTurtle with a red Pen
 #   and makes it move around a bit.  Don't forget to:
 #     -- import rosegraphics and construct a TurtleWindow
@@ -27,3 +27,29 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ###############################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+orange_turtle = rg.SimpleTurtle('turtle')
+orange_turtle.pen = rg.Pen('orange', 10)
+orange_turtle.speed = 20
+
+orange_turtle.pen_up()
+orange_turtle.left(90)
+orange_turtle.forward(300)
+orange_turtle.left(90)
+orange_turtle.forward(300)
+orange_turtle.pen_down()
+orange_turtle.left(90)
+orange_turtle.forward(600)
+orange_turtle.left(120)
+orange_turtle.forward(200)
+orange_turtle.right(20)
+orange_turtle.forward(300)
+orange_turtle.left(135)
+orange_turtle.forward(500)
+orange_turtle.right(125)
+orange_turtle.forward(300)
+
+window.close_on_mouse_click()
